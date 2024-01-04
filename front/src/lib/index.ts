f1ba638edaffd2745b16eb1256fa751bcb7eafc3
ClayenKitten <dima070704@yandex.ru>
2024-01-04 06:05:09 +0300

    Add settings dialog

--- front/src/lib/index.ts
+++ front/src/lib/index.ts
@@ -1,1 +1,17 @@
 export const BaseUrl = "http://5.35.88.225:8000/";
+
+export async function patchUserInfo(val: { rate: number }): Promise<void> {
+    let init: RequestInit = {
+        method: "PATCH",
+        body: JSON.stringify(val),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    await fetchAuthenticated("users/settings", init);
+}
+
+export async function fetchUserInfo(): Promise<{ rate: number }> {
+    let info = await (await fetchAuthenticated("users/settings")).json();
+    return info;
+}
