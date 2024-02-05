117be88630490914d3a5f03498ed7ce70fbf4901
ClayenKitten <dima070704@yandex.ru>
2024-02-05 12:59:52 +0300

    fix(front): remove special case for handling null in patched offers
    New editor doesn't allow writing null into a string field

--- front/src/lib/data/offers.ts
+++ front/src/lib/data/offers.ts
@@ -98,13 +98,6 @@ export async function fetchOfferList(): Promise<Offer[]> {
 }
 
 export async function patchOfferList(changed: Offer[]): Promise<void> {
-    for (const offer of changed) {
-        // Backend doesn't handle null values well, replace them with empty string before patching.
-        offer.note_1 = offer.note_1 ? offer.note_1 : "";
-        offer.note_2 = offer.note_2 ? offer.note_2 : "";
-        offer.note_3 = offer.note_3 ? offer.note_3 : "";
-    }
-
     await fetchAuthenticated("data/offers", {
         method: "PATCH",
         body: JSON.stringify(changed),
