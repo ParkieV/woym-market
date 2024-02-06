f923be6913a8d210feb283b47e8c80911a83832f
ClayenKitten <dima070704@yandex.ru>
2024-02-06 17:49:25 +0300

    fix(front): validate template inputs & rename properties

--- front/src/routes/app/templates/+page.svelte
+++ front/src/routes/app/templates/+page.svelte
@@ -99,6 +99,7 @@
                 display: flex;
                 flex-wrap: wrap;
                 justify-content: center;
+                align-items: start;
                 gap: 20px;
                 padding: 30px 0;
             }
