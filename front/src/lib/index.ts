c66f72c51c7e2b1ecf1468c7d8ede1957e10fe73
ClayenKitten <dima070704@yandex.ru>
2024-02-17 15:02:21 +0300

    feat(front): add taxes to settings page

--- front/src/lib/index.ts
+++ front/src/lib/index.ts
@@ -5,41 +5,56 @@ import { showLoadingModal, showNotification } from "./components/modal/Modals.sv
 
 export const BaseUrl = PUBLIC_BASE_URL;
 
-export async function handleRequest<R extends Response | Response[]>(
+export async function handleRequest<T, R extends Response | Response[]>(
     promise: Promise<R>,
-    modal?: {
-        header?: string;
-        errorHeader?: string;
-        onSuccess?: (response: Response) => void;
-    }
-): Promise<R> {
-    showLoadingModal(promise, modal?.header ?? "Загрузка...");
-    await promise.then(
-        async response => {
-            let responses = Array.isArray(response) ? response : [response];
-            for (const response of responses) {
-                if (response.status == 401) {
-                    logout();
-                    goto("/auth");
-                    showNotification(
-                        "Ошибка авторизации",
-                        "Пожалуйста, повторно войдите в аккаунт."
-                    );
-                } else if (response.status >= 500 && response.status <= 599) {
-                    showNotification("Ошибка сервера", "Не удалось достичь сервера.");
-                } else if (response.status >= 400 && response.status <= 499) {
-                    showNotification(
-                        modal?.errorHeader ?? "Ошибка",
-                        (await response.json()).detail
-                    );
-                } else if (response.ok) {
-                    modal?.onSuccess?.(response);
+    modal?: Partial<ModalSettings<T, R>>
+): Promise<T | undefined> {
+    let _modal = normalizeModal(modal ?? {});
+    showLoadingModal(promise, _modal.header);
+    return new Promise((resolve, reject) => {
+        promise.then(
+            async response => {
+                let responses: Response[] = Array.isArray(response) ? response : [response];
+                for (const response of responses) {
+                    if (response.ok) continue;
+                    if (response.status == 401) {
+                        logout();
+                        await goto("/auth");
+                        showNotification(
+                            "Ошибка авторизации",
+                            "Пожалуйста, повторно войдите в аккаунт."
+                        );
+                    } else if (response.status >= 500 && response.status <= 599) {
+                        showNotification("Ошибка сервера", "Не удалось достичь сервера.");
+                    } else if (response.status >= 400 && response.status <= 499) {
+                        showNotification(_modal.errorHeader, (await response.json()).detail);
+                    }
+                    reject();
+                    return;
                 }
+                resolve(_modal.onSuccess?.(response));
+            },
+            () => {
+                showNotification("Ошибка сети", "Не удалось достичь сервера.");
+                reject();
             }
-        },
-        () => {
-            showNotification("Ошибка сети", "Не удалось достичь сервера.");
-        }
-    );
-    return promise;
+        );
+    });
 }
+
+/** Sets default values for modal settings. */
+function normalizeModal<T, R extends Response | Response[]>(
+    partial: Partial<ModalSettings<T, R>>
+): ModalSettings<T, R> {
+    return {
+        header: partial.header ?? "Загрузка...",
+        errorHeader: partial.errorHeader ?? "Ошибка",
+        onSuccess: partial.onSuccess
+    };
+}
+
+type ModalSettings<T, R extends Response | Response[]> = {
+    header: string;
+    errorHeader: string;
+    onSuccess?: (response: R) => T;
+};
