7369bfd24750c70013feb8c04be4816cc93cfa60
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-02-19 21:47:14 +0300

    base ozon offers

--- app/src/api/base_api.py
+++ app/src/api/base_api.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from io import BytesIO
+
+import pandas as pd
+
 from src.schemas.base_api_schemas import APIOffer, APIWarehouse, APIPriceChangeData
 
 
@@ -19,4 +23,11 @@ class BaseAPI(ABC):
     @abstractmethod
     async def change_prices(self, data: list[APIPriceChangeData]) -> None:
         pass
+
+    def _download_report(self, url_path: str) -> pd.DataFrame:
+        output = BytesIO()
+        response = self.session.get(url_path)
+        output.write(response.content)
+        return pd.read_excel(output, engine='openpyxl')
+
 
---
+++ app/src/tests/ozon_api.py
@@ -0,0 +1,25 @@
+from src.api.ozon.api import OzonAPI
+import pytest
+
+api = OzonAPI(token='a66f89c1-73ef-487b-a051-f39875312fa2', entity_id=532844, shop_name='CALMAR.PLUS')
+
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+@pytest.mark.asyncio
+async def test_get_offers_identifiers():
+    ident = api._get_offers_identifiers()
+
+    assert ident
+
+    offers_info = api._get_offers_base_info(ident)
+    assert offers_info
+
+    offers_attributes = api._get_offers_attributes(ident)
+    assert offers_attributes
+
+    offers = await api.get_offers_list()
+    assert offers
+
+
