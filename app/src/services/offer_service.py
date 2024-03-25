7ceffbe1a8a28672909ba0a5736404038f5846c3
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-03-25 21:00:50 +0300

    fix(offers): handle dollar_cost_price_updated_at in pandas

--- app/src/services/offer_service.py
+++ app/src/services/offer_service.py
@@ -131,6 +131,7 @@ async def recalculate_values(session: AsyncSession, settings, which=None):
     df = pd.DataFrame([offer.model_dump() for offer in offers])
     df.drop('dollar_cost_price_updated_at', axis=1, inplace=True, errors='ignore')
     df.fillna(np.nan, inplace=True)
+    df.drop('dollar_cost_price_updated_at', axis=1, inplace=True, errors='ignore')
 
     if df.empty:
         return
