273069a13e22b4860695c6e6c0c496791760647f
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-07-31 06:27:28 +0300

    fix(offers): remove ';' sign from end of the search words

--- app/src/api/ozon/api.py
+++ app/src/api/ozon/api.py
@@ -210,7 +210,7 @@ class OzonAPI(BaseAPI):
 
                 search_attributes = [i for i in offer['attributes'] if i['attribute_id'] == 22336]
                 search_words = '; '.join([';'.join([words['value'] for words in item['values']]) for item in search_attributes])
-                search_words = search_words[:search_words[:256].rfind(';')+1]
+                search_words = search_words[:search_words[:256].rfind(';')]
 
                 result[offer['offer_id']] = {
                     'yandex_height': offer['height'] / unit_dimension_divider if offer['height'] else offer['height'],
