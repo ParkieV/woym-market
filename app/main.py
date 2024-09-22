304a27e392bf70a681e30260ca0b3fdbe0a0901a
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-09-22 15:27:39 +0300

    feat: add reset track fields
    Signed-off-by: NikitaKrylov <idoverchiviiloh@gmail.com>

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -25,6 +26,18 @@ from src.database.db import db_create
 import aioschedule
 from src.params.confing import config
 
+
+if config.use_sentry:
+    sentry_sdk.init(
+        dsn=config.sentry_sdk_dsn,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
 
 async def scheduler():
     aioschedule.every(60).minutes.do(update_data, 1)
