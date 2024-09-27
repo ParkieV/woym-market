9b95aeaa20ff9d7feca42253eeb59d3a03d629f0
NikitaKrylov <idoverchiviiloh@gmail.com>
2024-09-27 03:10:21 +0300

    feat: add update orders data to schedules
    Signed-off-by: NikitaKrylov <idoverchiviiloh@gmail.com>

--- app/scheduls.py
+++ app/scheduls.py
@@ -1,5 +1,6 @@
 from src.services.offer_service import update_offers
 from src.services.stocks_service import update_warehouses_and_stocks
+from src.services.orders_services import setup_orders
 from logs import get_logger
 
 logger = get_logger(__name__)
@@ -9,16 +10,16 @@ async def update_data(user_id: int):
     try:
         await update_offers(user_id)
     except Exception as e:
-        logger.error(f'Error in update offers', exc_info=True)
+        logger.error(f'Error in update offers', exc_info=e)
 
     try:
         await update_warehouses_and_stocks()
     except Exception as e:
-        logger.error(f"Error in update warehouses and stocks", exc_info=True)
+        logger.error(f"Error in update warehouses and stocks", exc_info=e)
 
-    # try:
-    #     await setup_catalog_items()
-    # except Exception as e:
-    #     logger.error(f"Error in setup catalog items", exc_info=True)
+    try:
+        await setup_orders()
+    except Exception as e:
+        logger.error(f'Error in update orders data', exc_info=e)
 
     logger.info('Schedules updated completed')
