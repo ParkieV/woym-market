a390e42ba01eab73532dd043a89d1676c48af6cb
ParkieV <st-psk@mail.ru>
2024-12-16 18:42:39 +0300

    add old front
    Signed-off-by: ParkieV <st-psk@mail.ru>

--- front/front.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:alpine AS builder
-WORKDIR /front
-COPY package.json .
-RUN npm install
-COPY . .
-RUN npm run build
--- front/src/routes/app/+page.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async () => {
-    console.log('Check redirect');
-    redirect(303, "/app/offers");
-};
