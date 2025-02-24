a3991006b1448d26ddb6925f679acbfed14a91e2
ParkieV <st-psk@mail.ru>
2025-02-24 12:37:33 +0300

    Fix discounts getting

--- backend/src/services/seller_discount.py
+++ backend/src/services/seller_discount.py
@@ -7,7 +7,7 @@ import pandas as pd
 def get_seller_discount_from_page(offers: pd.DataFrame) -> dict[str, float]:
     discounts = {}
     for offer in offers.itertuples():
-        if offer.auto_price_control:
+        if offer.auto_participation_in_promotions:
             if offer.seller_discount__api != offer.old_discount:
                 discounts[offer.id] = offer.seller_discount__api
             elif offer.seller_discount != offer.old_discount:
