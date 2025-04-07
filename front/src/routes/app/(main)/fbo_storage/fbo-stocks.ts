1f9b787de34a94ccfd05304c1f26628f39b9b8ff
ParkieV <st-psk@mail.ru>
2025-04-07 19:14:20 +0300

    Починил отображение значения 'К поставке' у складов

--- front/src/routes/app/(main)/fbo_storage/fbo-stocks.ts
+++ front/src/routes/app/(main)/fbo_storage/fbo-stocks.ts
@@ -100,7 +100,7 @@ export function calcToDeliver({
     is_deliver_in_boxes,
     in_box
 }: FboStocks): number {
-    let diff = Math.max(0, current_stock - min_stock);
+    let diff = Math.max(0, Math.abs(min_stock - current_stock));
     if (is_deliver_in_boxes) {
         let boxes_remainder = 0;
         if (diff % in_box !== 0) boxes_remainder = 1;
