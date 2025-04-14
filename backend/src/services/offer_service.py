7c01464b6fb1f30d707fbb69a270e2fedc57f62a
ParkieV <st-psk@mail.ru>
2025-04-14 13:44:08 +0300

    HotFix

--- backend/src/services/offer_service.py
+++ backend/src/services/offer_service.py
@@ -172,8 +172,11 @@ async def update_offers(db_session_fabric,
 
     # Создаем переменную с данными для отправки цен в апи
     to_update_price_df = db_offers_df[
-        (db_offers_df['auto_price_control'] == True) &
-        (db_offers_df['total_price'].notna())
+        (
+            (db_offers_df['sku'] == '28071') &
+            (db_offers_df['market'] == 'wildberries') &
+            (db_offers_df['name_of_shop'] == 'SkrabPlus')
+        )
         ][[
         'sku', 'market', 'name_of_shop', 'target_price',
         'manual_min_price', 'use_manual_min_price',
