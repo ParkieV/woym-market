5e9fed987cf6df2954722c414c795b02346b7910
ParkieV <st-psk@mail.ru>
2025-09-14 14:47:05 +0300

    Hotfix

--- backend/src/database/settings_db.py
+++ backend/src/database/settings_db.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.interfaces import ApiTypes
+from src.api.interfaces import ApiTypes
 from src.schemas import settings_schemas as schema
 from src.database.models.models import Logs, Settings, TableInfo, Market
 from src.schemas.settings_schemas import TableInfoOut, TableInfoCreate, MarketCreate, MarketOut, MarketFullOut, TableInfoUpdate
